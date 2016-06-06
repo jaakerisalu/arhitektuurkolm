afa1b361ca0631ea5bb9f70a525e6e0c0a370d7e
Jaak Erisalu <jaak.erisalu@gmail.com>
2016-06-07 00:24:31 +0300

    Add more error handling

--- arhitektuurkolm/arhitektuurkolm/admin.py
+++ arhitektuurkolm/arhitektuurkolm/admin.py
@@ -13,18 +13,35 @@ class SubjectAttributeInlineFormset(BaseGenericInlineFormSet):
             try:
                 if form.cleaned_data:
                     d = form.cleaned_data
-                    if d['attribute_type'].data_type == SubjectAttribute.TEXT:
-                        if any([d['value_date'] is not None, d['value_text'] == "",
-                                d['value_number'] is not None]):
-                            form.add_error('value_text', 'Please set TEXT and TEXT ONLY')
-                    if d['attribute_type'].data_type == SubjectAttribute.NUMBER:
-                        if any([d['value_date'] is not None, d['value_text'] is not "",
-                                d['value_number'] is None]):
-                            form.add_error('value_number', 'Please set NUMBER and NUMBER ONLY')
-                    if d['attribute_type'].data_type == SubjectAttribute.DATE:
-                        if any([d['value_date'] is None, d['value_text'] is not "",
-                                d['value_number'] is not None]):
-                            form.add_error('value_date', 'Please set DATE and DATE ONLY')
+                    # Validate the attribute type
+                    attribute_type_wrong = False
+
+                    # I apologize to the gods of Python for __class__.__name__ And everything else
+                    if (self.instance.__class__.__name__.lower() == "enterprise" and
+                            d['attribute_type'].belongs_to_type != SubjectAttribute.ENTERPRISE)\
+                        or (self.instance.__class__.__name__.lower() == "person" and
+                            d['attribute_type'].belongs_to_type != SubjectAttribute.PERSON)\
+                        or (self.instance.__class__.__name__.lower() == "employee" and
+                            d['attribute_type'].belongs_to_type != SubjectAttribute.EMPLOYEE)\
+                        or (self.instance.__class__.__name__.lower() == "customer" and
+                            d['attribute_type'].belongs_to_type != SubjectAttribute.CUSTOMER):
+                        form.add_error('attribute_type', 'That attribute cannot be used with the current parent object')
+                        attribute_type_wrong = True
+
+                    # Validate on the data types
+                    if not attribute_type_wrong:
+                        if d['attribute_type'].data_type == SubjectAttribute.TEXT:
+                            if any([d['value_date'] is not None, d['value_text'] == "",
+                                    d['value_number'] is not None]):
+                                form.add_error('value_text', 'Please set TEXT and TEXT ONLY')
+                        if d['attribute_type'].data_type == SubjectAttribute.NUMBER:
+                            if any([d['value_date'] is not None, d['value_text'] is not "",
+                                    d['value_number'] is None]):
+                                form.add_error('value_number', 'Please set NUMBER and NUMBER ONLY')
+                        if d['attribute_type'].data_type == SubjectAttribute.DATE:
+                            if any([d['value_date'] is None, d['value_text'] is not "",
+                                    d['value_number'] is not None]):
+                                form.add_error('value_date', 'Please set DATE and DATE ONLY')
             except AttributeError:
                 pass
 
