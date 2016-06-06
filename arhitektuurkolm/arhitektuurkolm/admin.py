0fb9bf38cc5399296958e3da0178d239dc999c81
Andres Antonen <Andres Antonen>
2016-06-07 00:05:38 +0300

    disable delete from admin

--- arhitektuurkolm/arhitektuurkolm/admin.py
+++ arhitektuurkolm/arhitektuurkolm/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib import messages
 
 from .models import *
 
@@ -10,53 +11,29 @@ class SubjectAttributeInline(GenericTabularInline):
     ct_fk_field = 'owner_fk'
     extra = 1
 
-
 class EnterpriseAdmin(admin.ModelAdmin):
     inlines = (SubjectAttributeInline, )
 
-    ''' EXAMPLES
-        def clean(self):
-            cleaned_data = super().clean()
-
-            if cleaned_data['start_hour'] >= cleaned_data['end_hour']:
-                raise ValidationError('We currently do not support tarifs that span midnight.')
-
-            return cleaned_data
-
-        def save(self, commit=True):
-            tarif = super().save(commit)
-
-            tarif.weekdays_bits = 0
-            for i in range(1, 8):
-                is_active = self.cleaned_data.get('weekday_%d' % i, False)
-                tarif.weekdays_bits += 2**i if is_active else 0
-
-            return tarif
-
-        ÜHE VÄLJA CLEAN MEETOD
-        def clean_displayed_units(self):
-            family = self.cleaned_data['family']
-            if self.cleaned_data['displayed_units'] and self.cleaned_data['displayed_units'] not in Metric.UNIT_CHOICES and self.cleaned_data['type'] == MetricsTypes.RealTimeMetric:
-                raise ValidationError("You cannot add a non-real time unit to a real time metric")
-            elif self.cleaned_data['displayed_units'] and self.cleaned_data['displayed_units'] not in family.get_units() and self.cleaned_data['type'] != MetricsTypes.RealTimeMetric:
-                raise ValidationError("There are no sensors with selected unit in the family. Please select one of %s" % family.get_units_display())
-            return self.cleaned_data["displayed_units"]
-
-        NSM LISA PARAMEETRID NTX
-        filter_horizontal = ['add', 'subtract'] # Add ja Subtract on ManyToMany väljad mudeli küljes
-
-        def sensors_to_add(self, obj):
-            return ", ".join([x.name for x in obj.add.all()])
-
-        def sensors_to_subtract(self, obj):
-            return ", ".join([x.name for x in obj.subtract.all()])
-
-        list_display = ('name', 'sensors_to_add', 'sensors_to_subtract', 'visible')
-    '''
-
+class PersonAdmin(admin.ModelAdmin):
+    def delete_model(self, request, obj):
+        """
+        Given a model instance delete it from the database.
+        """
+        if (obj.enterprise is not None):
+            self.message_user(request, "Cannot delete: this person is tied to an enterprise " + str(obj.enterprise), level=messages.ERROR)
+            return
+        obj.delete()
+        
+    def has_delete_permission(self, request, obj=None):
+        if (obj and obj.enterprise is not None):
+            self.message_user(request, "This person is tied to an enterprise " + str(obj.enterprise) + " and therefore cannot be deleted", level=messages.WARNING)
+            return False
+        return True
+    
+    
 admin.site.register(Address)
 admin.site.register(Employee)
 admin.site.register(EmployeeRole)
 admin.site.register(Enterprise, EnterpriseAdmin)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(SubjectAttribute)
