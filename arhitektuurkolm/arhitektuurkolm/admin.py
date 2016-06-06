c02fc4b9ec29451fc67fe392f40464f6aaae4a40
Jaak Erisalu <jaak.erisalu@gmail.com>
2016-06-06 23:56:37 +0300

    Add type-based validation to SubjectAttributes

--- arhitektuurkolm/arhitektuurkolm/admin.py
+++ arhitektuurkolm/arhitektuurkolm/admin.py
@@ -1,7 +1,32 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.contenttypes.forms import BaseGenericInlineFormSet
+from django.core.exceptions import ValidationError
 
 from .models import *
+
+
+class SubjectAttributeInlineFormset(BaseGenericInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            try:
+                if form.cleaned_data:
+                    d = form.cleaned_data
+                    if d['attribute_type'].data_type == SubjectAttribute.TEXT:
+                        if any([d['value_date'] is not None, d['value_text'] == "",
+                                d['value_number'] is not None]):
+                            form.add_error('value_text', 'Please set TEXT and TEXT ONLY')
+                    if d['attribute_type'].data_type == SubjectAttribute.NUMBER:
+                        if any([d['value_date'] is not None, d['value_text'] is not "",
+                                d['value_number'] is None]):
+                            form.add_error('value_number', 'Please set NUMBER and NUMBER ONLY')
+                    if d['attribute_type'].data_type == SubjectAttribute.DATE:
+                        if any([d['value_date'] is None, d['value_text'] is not "",
+                                d['value_number'] is not None]):
+                            form.add_error('value_date', 'Please set DATE and DATE ONLY')
+            except AttributeError:
+                pass
 
 
 class SubjectAttributeInline(GenericTabularInline):
@@ -9,54 +34,36 @@ class SubjectAttributeInline(GenericTabularInline):
     ct_field = 'owner_type'
     ct_fk_field = 'owner_fk'
     extra = 1
+    formset = SubjectAttributeInlineFormset
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        print(qs)
+        return qs
+
+
+class EnterpriseForm(forms.ModelForm):
+    class Meta:
+        model = Enterprise
+        exclude = []
 
 
 class EnterpriseAdmin(admin.ModelAdmin):
     inlines = (SubjectAttributeInline, )
+    form = EnterpriseForm
 
-    ''' EXAMPLES
-        def clean(self):
-            cleaned_data = super().clean()
 
-            if cleaned_data['start_hour'] >= cleaned_data['end_hour']:
-                raise ValidationError('We currently do not support tarifs that span midnight.')
+class PersonAdmin(admin.ModelAdmin):
+    inlines = (SubjectAttributeInline,)
 
-            return cleaned_data
 
-        def save(self, commit=True):
-            tarif = super().save(commit)
+class EmployeeAdmin(admin.ModelAdmin):
+    inlines = (SubjectAttributeInline,)
 
-            tarif.weekdays_bits = 0
-            for i in range(1, 8):
-                is_active = self.cleaned_data.get('weekday_%d' % i, False)
-                tarif.weekdays_bits += 2**i if is_active else 0
-
-            return tarif
-
-        ÜHE VÄLJA CLEAN MEETOD
-        def clean_displayed_units(self):
-            family = self.cleaned_data['family']
-            if self.cleaned_data['displayed_units'] and self.cleaned_data['displayed_units'] not in Metric.UNIT_CHOICES and self.cleaned_data['type'] == MetricsTypes.RealTimeMetric:
-                raise ValidationError("You cannot add a non-real time unit to a real time metric")
-            elif self.cleaned_data['displayed_units'] and self.cleaned_data['displayed_units'] not in family.get_units() and self.cleaned_data['type'] != MetricsTypes.RealTimeMetric:
-                raise ValidationError("There are no sensors with selected unit in the family. Please select one of %s" % family.get_units_display())
-            return self.cleaned_data["displayed_units"]
-
-        NSM LISA PARAMEETRID NTX
-        filter_horizontal = ['add', 'subtract'] # Add ja Subtract on ManyToMany väljad mudeli küljes
-
-        def sensors_to_add(self, obj):
-            return ", ".join([x.name for x in obj.add.all()])
-
-        def sensors_to_subtract(self, obj):
-            return ", ".join([x.name for x in obj.subtract.all()])
-
-        list_display = ('name', 'sensors_to_add', 'sensors_to_subtract', 'visible')
-    '''
 
 admin.site.register(Address)
-admin.site.register(Employee)
+admin.site.register(Employee, EmployeeAdmin)
 admin.site.register(EmployeeRole)
 admin.site.register(Enterprise, EnterpriseAdmin)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(SubjectAttribute)
